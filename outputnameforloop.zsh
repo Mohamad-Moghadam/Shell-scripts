5e6e5bced80895b65e0e1e7ee482959497f8c457
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-11-07 17:20:54 +0330

    🎉 second for loop script added

---
+++ outputnameforloop.zsh
@@ -0,0 +1,11 @@
+#!/bin/zsh
+# Author: Moe
+# Date: 07/11/2025
+# Description: second for loop shell script
+# Date Modified: 07/11/2025
+#
+echo
+for i in eat run jump play
+do
+	echo See Moe $i
+done
