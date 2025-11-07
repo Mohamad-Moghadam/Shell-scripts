a6a45ae70510adb0e9af2363e9e977a8a84f7986
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-11-07 17:08:34 +0330

    🎉 for loop script added

---
+++ forloop.zsh
@@ -0,0 +1,13 @@
+#!/bin/zsh
+# Author: Moe
+# Date: 07/11/2025
+# Description: Just a shell script, so I could learn for loops in shell env
+# Date Modified: 07/11/2025
+
+echo
+echo forst loop program started
+echo
+for i in 1 2 3 4 5
+do
+	echo "welcome $i times"
+done
